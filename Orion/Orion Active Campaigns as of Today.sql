c13bde02df1388c858fb02331074eb6646a1d331
balaserawat <159872997+balaserawat@users.noreply.github.com>
2026-08-19 11:15:39 +0530

    Add files via upload

---
+++ Orion/Orion Active Campaigns as of Today.sql
@@ -0,0 +1,38 @@
+ select
+        id as campaign_id,
+        store_ids[0] as store_id,
+        absolute_rdgmv_budget,
+        absolute_sdgmv_budget,
+        updated_by,
+        to_date(from_unixtime(created_at)) as created_on,
+        to_date(from_unixtime(start_time)) as start_dt,
+        to_date(from_unixtime(end_time)) as end_dt,
+        rdgmv_percentage_tolerance_v2.target_value as rdgmv_target_value,
+        sdgmv_percentage_tolerance_v2.target_value as sdgmv_target_value,
+        rdgmv_percentage_tolerance_v2.lower_tolerance as rdgmv_target_LT,
+        sdgmv_percentage_tolerance_v2.lower_tolerance as sdgmv_target_LT,
+        rdgmv_percentage_tolerance_v2.upper_tolerance as rdgmv_target_UT,
+        sdgmv_percentage_tolerance_v2.upper_tolerance as sdgmv_target_UT,
+        campaign_status,
+        consent_status,
+        from_unixtime(created_at) as created_at,
+        smart_discount_enabled
+    from (
+        select
+            *,
+            row_number() over (
+                partition by id
+                order by dt desc, hr desc, updated_at desc
+            ) as rn
+        from prod.streams_delta.growth_campaign_crud_event
+    ) x
+    where rn = 1
+    and to_date(from_unixtime(start_time)) <= current_date
+    and to_date(from_unixtime(end_time)) > current_date
+    and campaign_status = 'Growth_CAMPAIGN_STATUS_ACTIVE'
+    and consent_status = 'Growth_CAMPAIGN_CONSENT_STATUS_APPROVED'
+    and smart_discount_enabled = true
+
+
+
+    --Growth_CAMPAIGN_CONSENT_STATUS_APPROVED
