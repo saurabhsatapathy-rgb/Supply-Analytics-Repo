25a52628045ace3681e75186e0e01e6532a1d8f9
balaserawat <159872997+balaserawat@users.noreply.github.com>
2026-08-28 13:12:00 +0530

    Refactor SQL query for active campaigns

--- Orion/Orion Active Campaigns as of Today.sql
+++ Orion/Orion Active Campaigns as of Today.sql
@@ -1,9 +1,14 @@
- select
-        id as campaign_id,
-        store_ids[0] as store_id,
+
+select id as campaign_id,
+        store_id_val as store_id,
+        store_ids,
         absolute_rdgmv_budget,
         absolute_sdgmv_budget,
         updated_by,
+        smart_discount_enabled,
+        updated_by,
+        from_unixtime(updated_at) as updated_at,
+        to_date(from_unixtime(updated_at)) as updated_at_dt,
         to_date(from_unixtime(created_at)) as created_on,
         to_date(from_unixtime(start_time)) as start_dt,
         to_date(from_unixtime(end_time)) as end_dt,
@@ -13,10 +18,9 @@
         sdgmv_percentage_tolerance_v2.lower_tolerance as sdgmv_target_LT,
         rdgmv_percentage_tolerance_v2.upper_tolerance as rdgmv_target_UT,
         sdgmv_percentage_tolerance_v2.upper_tolerance as sdgmv_target_UT,
-        campaign_status,
-        consent_status,
-        from_unixtime(created_at) as created_at,
-        smart_discount_enabled
+        nbh_sdgmv_percentage_tolerance.target_value as nbh_sdgmv_target_value,
+        nbh_sdgmv_percentage_tolerance.lower_tolerance as nbh_sdgmv_target_LT,
+        nbh_sdgmv_percentage_tolerance.upper_tolerance as nbh_sdgmv_target_UT
     from (
         select
             *,
@@ -25,10 +29,10 @@
                 order by dt desc, hr desc, updated_at desc
             ) as rn
         from prod.streams_delta.growth_campaign_crud_event
+        qualify rn = 1
     ) x
-    where rn = 1
-       AND FROM_UNIXTIME(start_time) <= current_timestamp
+    LATERAL VIEW OUTER EXPLODE(x.store_ids) s AS store_id_val
+    where FROM_UNIXTIME(start_time) <= current_timestamp
             AND FROM_UNIXTIME(end_time) >= current_timestamp
     and campaign_status = 'Growth_CAMPAIGN_STATUS_ACTIVE'
-    -- and consent_status = 'Growth_CAMPAIGN_CONSENT_STATUS_APPROVED'
     and smart_discount_enabled = true
